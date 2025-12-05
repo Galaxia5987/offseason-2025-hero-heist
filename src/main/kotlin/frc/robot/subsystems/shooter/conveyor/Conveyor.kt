c899f23bf827a743e0f1a3a4aead4df9dce9fba0
rakrakon <rakrakon@gmail.com>
2025-12-05 12:29:46 +0200

    something that not suppose to be there

--- src/main/kotlin/frc/robot/subsystems/shooter/conveyor/Conveyor.kt
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.shooter.conveyor
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.lib.universal_motor.UniversalTalonFX
-
-object Conveyor: SubsystemBase(){
-    val motor = UniversalTalonFX()
-}
--- src/main/kotlin/frc/robot/subsystems/shooter/flywheel/Flywheel.kt
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.shooter.flywheel
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase
-
-object Flywheel: SubsystemBase() {
-
-}
