cb355481a200ff30cb44d58f1880c2a1b55f67f7
rakrakon <rakrakon@gmail.com>
2025-12-12 10:39:06 +0200

    add real gains

--- src/main/kotlin/frc/robot/subsystems/climb/constants.kt
+++ src/main/kotlin/frc/robot/subsystems/climb/constants.kt
@@ -2,6 +2,7 @@ package frc.robot.subsystems.climb
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
+import com.ctre.phoenix6.configs.Slot0Configs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
@@ -22,6 +23,11 @@ val CONFIG = TalonFXConfiguration().apply {
             SupplyCurrentLimit = 80.0
             StatorCurrentLimitEnable = true
             StatorCurrentLimit = 40.0
+        }
+    Slot0 =
+        Slot0Configs().apply {
+            kP = 1.0
+            kD = 0.0
         }
 }
 
